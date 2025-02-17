04e2738d0d044e37c1c08706c74b7b678a5c4e92
Sergio260837 <sergiosierrajaramillo@gmail.com>
2025-02-17 09:02:55 +0100

    PASO 2-Bienvenido a la broma

--- broma.sh
+++ broma.sh
@@ -2,3 +2,8 @@
 echo "🚀 Iniciando la broma épica..."
 mkdir -p ~/broma_inicio
 cd ~/broma_inicio
+
+if [ ! -d ".git" ]; then
+    git init
+    echo "Bienvenido al repo de la broma! 😆" > readme.md
+fi
