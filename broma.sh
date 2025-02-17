0eda1a08c544f8a3d83a835b231a97062ad6c0f3
Sergio260837 <sergiosierrajaramillo@gmail.com>
2025-02-17 09:05:30 +0100

    PASO 5-Crear el script para que se ejecute

--- broma.sh
+++ broma.sh
@@ -13,3 +13,11 @@ curl -o imagen_graciosa.jpg https://s2.abcstatics.com/Media/201505/07/pantallaaz
 
 echo "🔊 Configurando el sonido de la broma..."
 cp "/mnt/data/Windows Error Remix.mp3" "$HOME/broma_inicio/sonido_broma.mp3"
+
+echo "📜 Creando el script de broma..."
+echo '#!/bin/bash' > broma.sh
+echo 'xdg-open "$HOME/broma_inicio/imagen_graciosa.jpg" & ' >> broma.sh
+echo 'notify-send "¡Buenos días, $(whoami)! 🌞" "Tu día comienza con un toque de humor y locura 😂"' >> broma.sh
+echo 'paplay "$HOME/broma_inicio/sonido_broma.mp3" &' >> broma.sh  # Usa `paplay` si está disponible
+echo 'echo "😂 ¡Te atrapamos! Ahora disfrútalo mientras trabajas (o procrastinas)!"' >> broma.sh
+chmod +x broma.sh
