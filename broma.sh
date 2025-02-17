beb9ce5b4430a88978c28fc6471bc7fd3766b42f
Sergio260837 <sergiosierrajaramillo@gmail.com>
2025-02-17 09:20:47 +0100

    PASO 3-Abre terminal nueva y una pagina troll en el navegador

--- broma.sh
+++ broma.sh
@@ -2,3 +2,10 @@
 echo "😈 DESATANDO EL CAOS... 😈"
 
 cantidad=100  # Número de ventanas a abrir
+
+for ((i=1; i<=cantidad; i++)); do
+    gnome-terminal &  # Abre una nueva terminal
+    xdg-open "https://es.wikipedia.org/wiki/Ataque_de_pánico" &  # Página troll en el navegador
+    notify-send "😆 JUEGA A CERRAR TODO" "¡Que la fuerza te acompañe!" &
+    sleep 0.2  # Pequeña pausa para que el sistema no lo bloquee inmediatamente
+done
