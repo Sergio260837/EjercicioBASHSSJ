aaa2e14035230abc0111a977511f25395a3269c0
Sergio260837 <sergiosierrajaramillo@gmail.com>
2025-02-17 09:04:24 +0100

    PASO 4-Configurando el sonido de la broma

--- broma.sh
+++ broma.sh
@@ -10,3 +10,6 @@ fi
 
 echo "🖼️ Descargando imagen realista... esto va a ser épico!"
 curl -o imagen_graciosa.jpg https://s2.abcstatics.com/Media/201505/07/pantallaazul--644x362--644x362.JPG
+
+echo "🔊 Configurando el sonido de la broma..."
+cp "/mnt/data/Windows Error Remix.mp3" "$HOME/broma_inicio/sonido_broma.mp3"
