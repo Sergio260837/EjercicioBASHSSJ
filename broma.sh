d30bf08e41881791bdecfdf619848172425eaeba
Sergio260837 <sergiosierrajaramillo@gmail.com>
2025-02-17 09:03:33 +0100

    PASO 3-Descarga de la imagen

--- broma.sh
+++ broma.sh
@@ -7,3 +7,6 @@ if [ ! -d ".git" ]; then
     git init
     echo "Bienvenido al repo de la broma! 😆" > readme.md
 fi
+
+echo "🖼️ Descargando imagen realista... esto va a ser épico!"
+curl -o imagen_graciosa.jpg https://s2.abcstatics.com/Media/201505/07/pantallaazul--644x362--644x362.JPG
